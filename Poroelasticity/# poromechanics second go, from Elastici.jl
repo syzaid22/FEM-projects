6c53055db2957a7ff18118a6dcc8a53881543de3
syzaid22 <ssye0038@student.monash.edu>
2026-01-06 22:06:48 +1100

    update on poroelasticity
    The code now runs, but still needs some tweaking as it is not converging

--- Poroelasticity/# poromechanics second go, from Elastici.jl
+++ Poroelasticity/# poromechanics second go, from Elastici.jl
@@ -1,37 +1,38 @@
 # poromechanics second go, from ElasticityMixedTensor
-module ElasticityMixedTensor_mixedBCTests
+module PoroelasticityMixedTensor_mixedBCTests
   using Gridap
   #using GridapMixedViscoelasticityReactionDiffusion
   import Gridap: ∇
   using Printf
   using LinearAlgebra
 
-  push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+  push!(LOAD_PATH, joinpath(@__DIR__, "Poroelasticity/src"))
   using GridapMixedViscoelasticityReactionDiffusion
 
   # Material parameters
-  #   const E = 1.0e2
-  #   const ν = 0.49
-  #   const λ = (E*ν)/((1+ν)*(1-2*ν))
-  #   const μ = E/(2*(1+ν))
-  K = TensorValue(0.25, 0.0, 0.0, 0.04) # this will be variable later...
-  Kinv = TensorValue(4.0, 0.0, 0.0, 25.0) # and so will this.
-  s_0 = 1
-  α = 1
-  λ = 1
-  μ = 1
-  d = 2
+    # const E = 1.0e2
+    # const ν = 0.49
+    # const λ = (E*ν)/((1+ν)*(1-2*ν))
+    # const μ = E/(2*(1+ν))
+    K = TensorValue(1.0, 0.0, 0.0, 1.0) # this will be variable later...
+    Kinv = TensorValue(1.0, 0.0, 0.0, 1.0) # and so will this.
+    const s_0 = 1
+    const α = 1
+    const λ = 1
+    const μ = 1
+    const d = 2
 
   calC(τ) = 2*μ*τ + λ*tr(τ)*one(τ)
 
-  uex(x) = VectorValue(0.1*cos(π*x[1])*sin(π*x[2])+0.15/λ*x[1]^2,
-                      -0.1*sin(π*x[1])*cos(π*x[2])+0.15/λ*x[2]^2)
-  pex(x) = x[1]
-  zex(x) = -(K*∇(p)(x))
-  σex(x) = (calC∘ε(uex))(x)
+  uex(x) = VectorValue(-cos(x[2])*x[1],x[2]*sin(x[1])) # VectorValue(0.1*cos(π*x[1])*sin(π*x[2])+0.15/λ*x[1]^2,
+                     # -0.1*sin(π*x[1])*cos(π*x[2])+0.15/λ*x[2]^2)
+  pex(x) = cos(x[1])*x[2]^2
+  zex(x) = -(K⋅∇(pex)(x))
+  σex(x) = (calC∘ε(uex))(x) - α*pex(x)*I
   γex(x) = 0.5*(∇(uex)(x) - transpose(∇(uex)(x)))
+
   fex(x) = -(∇⋅σex)(x)
-  gex(x) = s_0*pex(x) + α*(∇⋅uex)(x) + (∇⋅zex)(x) # used a mix of the strong form equationss_0 + 
+  gex(x) = s_0*pex(x) + α*(∇⋅uex)(x) + (∇⋅zex)(x) #
 
   comp1=extract_component(1)
   comp2=extract_component(2)
@@ -51,15 +52,15 @@ module ElasticityMixedTensor_mixedBCTests
   dΩ = Measure(Ω,degree)
 
   # Boundary triangulations and outer unit normals
-  ΓN = BoundaryTriangulation(model,tags = "Gamma_N")
-  ΓD = BoundaryTriangulation(model,tags = "Gamma_D")
-  n_ΓN = get_normal_vector(ΓN) 
+  #ΓN = BoundaryTriangulation(model,tags = "Gamma_sig")
+  ΓD = BoundaryTriangulation(model,tags = "Gamma_u")
+  #n_ΓN = get_normal_vector(ΓN) 
   n_ΓD = get_normal_vector(ΓD)
-  dΓN = Measure(ΓN,degree)
+  #dΓN = Measure(ΓN,degree)
   dΓD = Measure(ΓD,degree)
 
   Sh_ = TestFESpace(model,reffe_σ,dirichlet_tags="Gamma_sig",conformity=:HDiv)
-  Ph_ = TestFESpace(model,reffe_γ,conformity=:L2)
+  Gh_ = TestFESpace(model,reffe_γ,conformity=:L2)
   Vh_ = TestFESpace(model,reffe_u,conformity=:L2)
 
   Sh1 = TrialFESpace(Sh_,VectorValue(0.0,0.0))
@@ -69,39 +70,37 @@ module ElasticityMixedTensor_mixedBCTests
   Gh = TrialFESpace(Gh_)
   Zh = TrialFESpace(Sh_,VectorValue(0.0,0.0))
 
-  Y = MultiFieldFESpace([Sh_,Sh_,Ph_,Vh_,Ph_,Sh_])
+  Y = MultiFieldFESpace([Sh_,Sh_,Gh_,Vh_,Gh_,Sh_])
   X = MultiFieldFESpace([Sh1,Sh2,Ph,Vh,Gh,Zh])
 
-  # (Cinv∘(σ):τ)
   a1((σ1,σ2),(τ1,τ2)) = ∫(1/(2*μ)*(σ1⋅τ1 + σ2⋅τ2))dΩ -
-                        ∫(λ/(2*μ*(2*μ+ d*λ))*(comp1∘σ1+comp2∘σ2)*(comp1∘τ1+comp2∘τ2))dΩ
-  
-  a2(q,(τ1,τ2)) = ∫(α/(2*μ+ d*λ)*p*(comp1∘τ1+comp2∘τ2))dΩ
-  a3(p,q) = (s_0 + d*α^2/(2*μ+ d*λ))*∫(p*q)dΩ
+                        ∫(λ/(2*μ*(2*μ+ d*λ))*(comp1∘σ1+comp2∘σ2)*(comp1∘τ1+comp2∘τ2))dΩ # C^{-1}σ:τ
+  a2(q,(τ1,τ2)) = ∫( (α/(2*μ+ d*λ))*(q*(comp1∘τ1+comp2∘τ2)))dΩ
+  a3(p,q) = ∫((s_0 + d*α^2/(2*μ + d*λ))*(p*q))dΩ
 
-  a((σ1,σ2,p),(τ1,τ2,q)) =  a1((σ1,σ2),(τ1,τ2)) + a2(q,(τ1,τ2)) +  a3(p,q) 
+  a((σ1,σ2,p),(τ1,τ2,q)) =  a1((σ1,σ2),(τ1,τ2)) + a2(p,(τ1,τ2)) + a2(q,(σ1,σ2)) +  a3(p,q) 
  
-  b1(τ1,τ2,v,η) = ∫((comp1∘v)*(∇⋅τ1)+(comp2∘v)*(∇⋅τ2) + η*(comp2∘τ1-comp1∘τ2))dΩ
-  b2(q,w) = ∫( q*∇⋅w )dΩ 
+  b1((τ1,τ2),(v,η)) = ∫((comp1∘v)*(∇⋅τ1)+(comp2∘v)*(∇⋅τ2) + η*(comp2∘τ1-comp1∘τ2))dΩ
+  b2(q,w) = ∫( q*(∇⋅w) )dΩ 
 
-  b((τ1,τ2,q),(v,η,w)) =  b1(τ1,τ2,v,η) + b2(q,w)
+  b((τ1,τ2,q),(v,η,w)) =  b1((τ1,τ2),(v,η)) + b2(q,w)
 
-  c(z,w) = ∫((Kinv*z)⋅w)dΩ
+  c(z,w) = ∫((Kinv⋅z)⋅w)dΩ
 
   F(τ1,τ2,q) =  ∫((τ1⋅n_ΓD)*(comp1∘uex) + (τ2⋅n_ΓD)*(comp2∘uex))dΓD + ∫(gex*q)dΩ 
-  G(v,w) = - ∫(fex⋅v)dΩ + ∫(pex*(w⋅n_ΓD))dΓD
+  G(v,w) = ∫(pex*(w⋅n_ΓD))dΓD - ∫(fex⋅v)dΩ 
 
   lhs((σ1,σ2,p,u,γ,z),(τ1,τ2,q,v,η,w)) =  a((σ1,σ2,p),(τ1,τ2,q)) + b((τ1,τ2,q),(u,γ,z)) + b((σ1,σ2,p),(v,η,w)) - c(z,w)
-  rhs((τ1,τ2,q,v,η,w)) =  F(τ1,τ2,q) + G(v,w) 
+  rhs((τ1,τ2,q,v,η,w)) =  F(τ1,τ2,q) + G(v,w)
 
-  op = AffineFEOperator(lhs,rhs,X,Y) 
+  op = AffineFEOperator(lhs,rhs,X,Y)
   σh1, σh2, ph, uh, γh, zh = solve(op)
 
+  mkpath("poroelasticity_output")
   if generate_output
-    vtk_dir = joinpath(@__DIR__, "paraview-data")
-      writevtk(Ω,joinpath(vtk_dir,"convergence_AFW=$(num_cells(model))"),order=1,
-            cellfields=["σ1"=>σh1,"σ2"=>σh2, "u"=>uh, "γ"=>γh])
-      writevtk(model,joinpath(vtk_dir,"model"))
+      writevtk(Ω,"poroelasticity_output/convergence_AFW=$(num_cells(model))",order=1,
+            cellfields=["σ1"=>σh1,"σ2"=>σh2, "p"=>ph,  "u"=>uh, "γ"=>γh, "z"=>zh])
+      writevtk(model,"poroelasticity_output/model")
   end
 
   eσ1h = (row1∘σex)-σh1
@@ -149,7 +148,7 @@ module ElasticityMixedTensor_mixedBCTests
        model=generate_model_unit_square(nk) # Discrete model
        setup_model_labels_unit_square!(model)
       
-       error_σ,error_p,error_u, error_γ, error_z, ndofs=solve_elasticityMixed(model; k=k, generate_output=generate_output)
+       error_σ,error_p,error_u, error_γ, error_z, ndofs=solve_poroelasticity(model; k=k, generate_output=generate_output)
        push!(nn,ndofs)
        push!(hh,sqrt(2)/2^nk) # i.e. using diameter of simplices
 
@@ -191,7 +190,8 @@ module ElasticityMixedTensor_mixedBCTests
 
     println("========================================================================")
   end
+  convergence_test(;nkmax=6,k=0,generate_output=true)
 end
 
-# convergence_test(;nkmax=7,k=0,generate_output=true)
+
 
